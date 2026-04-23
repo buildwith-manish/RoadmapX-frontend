21753c542d661db89df828032ecdd3772944a916
manishnkotian11 <manishnkotian11@gmail.com>
2026-04-24 03:18:42 +0530

    Fix auth_guard to use session

--- auth_guard.js
+++ auth_guard.js
@@ -1,5 +1,13 @@
-(function () {
-  if (!localStorage.getItem("rx_token")) {
+(async function () {
+  try {
+    const res = await fetch("https://roadmapx-backend-3qmc.onrender.com/me", {
+      credentials: "include"
+    });
+    const data = await res.json();
+    if (!data.success) {
+      window.location.replace("login.html");
+    }
+  } catch (e) {
     window.location.replace("login.html");
   }
 })();
